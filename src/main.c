6ba497e36f36394c01794243ddb66e7114b3f935
Satya Pusuluri <sbp31@duke.edu>
2023-03-02 20:16:34 -0500

    created overlay with alises for LEDs

--- src/main.c
+++ src/main.c
@@ -13,30 +13,41 @@ LOG_MODULE_REGISTER(Lab8_Satya, LOG_LEVEL_DBG);
 #define SLEEP_TIME_MS   1000
 
 /* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
+#define LED0_NODE DT_ALIAS(heartbeat)
+#define LED1_NODE DT_ALIAS(buzzer)
+#define LED2_NODE DT_ALIAS(ivdrip)
+#define LED3_NODE DT_ALIAS(alarm)
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec heartbeat_led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec buzzer_led = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec ivdrip_led = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+static const struct gpio_dt_spec alarm_led = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
 
 void main(void)
 {
 	int ret;
 
-	if (!device_is_ready(led.port)) {
+	if (!device_is_ready(heartbeat_led.port)) {
 		return;
 	}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	ret = gpio_pin_configure_dt(&heartbeat_led, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
 		return;
 	}
 
 	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		LOG_INF("Heartbeat");
+		ret = gpio_pin_toggle_dt(&buzzer_led);
+		/*
+		ret = gpio_pin_toggle_dt(&buzzer_led);
+		ret = gpio_pin_toggle_dt(&ivdrip_led);
+		ret = gpio_pin_toggle_dt(&alarm_led);
+		*/
+		LOG_INF("Everything is blinking!");
 		if (ret < 0) {
 			return;
 		}
